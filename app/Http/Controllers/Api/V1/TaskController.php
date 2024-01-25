7e807cec255581441a8338b8fc4bf1633eceb74f
samih93 <samih_damaj@hotmail.com>
2024-01-25 11:47:47 -0800

    initial commit

---
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
+use App\Http\Requests\storeTaskRequest;
+use App\Http\Requests\updateTaskRequest;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return TaskResource::collection(Task::all());
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(storeTaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+        return TaskResource::make($task);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(updateTaskRequest $request, Task $task)
+    {
+        //
+        $task->update($request->validated());
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+        $task->delete();
+        return response()->noContent();
+    }
+}
